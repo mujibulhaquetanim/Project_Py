b227678b39d630b7d1a0fcec8a23cc71f479d3dc
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-15 13:18:54 +0600

    os module,STATICFILES_DIR added in settings & shortcuts added for rendering static files

--- Django/sampleProject/sampleProject/views.py
+++ Django/sampleProject/sampleProject/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello from the home page")
+    return render(request, 'website/index.html')
+    # return HttpResponse("Hello from the home page")
 
 def contact(request):
-    return HttpResponse("Hello from contact page")
+    # return HttpResponse("Hello from contact page")
+    return render(request, 'website/contact.html')
 
 def about(request):
-    return HttpResponse("Hi from the about page")
+    # return HttpResponse("Hi from the about page")
+    return render(request, 'website/about.html')
