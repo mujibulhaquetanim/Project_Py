640bd6608af7b93a4bc4ca79cc92a21f16860f40
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-20 21:52:36 +0600

    npm_bin_path added and tailwind installed

--- Django/sampleProject/sampleProject/settings.py
+++ Django/sampleProject/sampleProject/settings.py
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
     'theme',
 ]
 
-TAILWIND_APP_NAME = 'theme'
-INTERNAL_IPS = ['127.0.0.1']
+TAILWIND_APP_NAME = 'theme';
+INTERNAL_IPS = ['127.0.0.1'];
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
