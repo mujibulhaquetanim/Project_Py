1d9796af917b6d1d41e4c6b6d74940baa6d6d0ae
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-23 23:13:08 +0600

    root path added for new app 'chai'

--- Django/sampleProject/chai/urls.py
+++ Django/sampleProject/chai/urls.py
@@ -2,7 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    
-    
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('',views.all_chai,name='all_chai')
+
 ]
