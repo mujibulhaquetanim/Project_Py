5fe34affc055df5293c8541b5a9f3357c2a73529
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-22 22:25:03 +0600

    create two dummy routes and pass obj, created with obj notation and with class keyword

--- fastAPI/src/Crud_http_method/books.py
+++ fastAPI/src/Crud_http_method/books.py
@@ -4,6 +4,24 @@ app = FastAPI()
 
 Books = [{'title': 'title one', 'author': 'author one', 'category': 'science'},{'title': 'title two', 'author': 'author two', 'category': 'fiction'},{'title': 'title three', 'author': 'author three', 'category': 'thriller'},{'title': 'title four', 'author': 'author four', 'category': 'philosophy'},{'title': 'title five', 'author': 'author five', 'category': 'drama'}];
 
+class Book:
+    id: int
+    name: str
+    
+    def __init__(self, id, name):
+        self.id = id,
+        self.name = name
+
+classBook = [
+    Book(1,"book1"),
+    Book(2,'book2')
+]
+
+obj = [
+    {"name": "kire"},
+    {"name": "oi"}
+]
+
 @app.get("/")
 async def first_api():
     return {"message": "Hello from FastAPI! hit '/docs' end-point to see all of the routes"}
@@ -11,3 +29,12 @@ async def first_api():
 @app.get("/books")
 async def books():
     return Books;
+
+@app.get("/wow")
+async def wow():
+    return "oi kire oi kire"
+
+#returning list consist of two types of obj, created by class and object notation.
+@app.get("/class")
+async def classBook1():
+    return [obj,classBook];
